6000382abc6ad0c3b1d9eae501920371a944aa3e
Jefferson Ferreira <93939600+Jefferson-F-Ribeiro@users.noreply.github.com>
2022-02-12 02:54:13 +0000

    Preparing the main function.

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include "array_list_int.h"
 
 int main(void) {
-  printf("Hello World\n");
+  int erro;
+  
+  array_list_int *lista = array_list_create();
+  printf("%d\n",array_list_size(lista));
+  array_list_push_back(lista,10);
+  printf("%d\n",array_list_size(lista));
+  printf("%d\n",array_list_get(lista,0,&erro));
   return 0;
 }
